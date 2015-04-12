07a5b920de72648e17631527a4881872b974d8f1
Caleb Evans <caleb@calebevans.me>
2015-04-12 14:08:55 -0700

    Recognize and parse timestamped filenames

--- src/local.sh
+++ src/local.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 # Script to execute on local machine
 
+# Parse the given configuration file
 config_path="$1"
 program_dir="$(dirname "${BASH_SOURCE[0]}")"
 source "$program_dir"/deps/read_ini.sh
 read_ini "$config_path" --prefix config
 
-# Expand ~ to full home directory path for the given path
+# Expands ~ to full home directory path
 expand_home() {
 	echo "${1/#~/$HOME}"
 }
+config__paths__remote_backup="$(date +"$config__paths__remote_backup")"
+config__paths__local_backup="$(date +"$config__paths__local_backup")"
 config__paths__local_backup="$(expand_home "$config__paths__local_backup")"
 
-# If all necessary configuration variables have been provided
-if [[ ! -z $config__paths__wordpress && ! -z $config__paths__remote_backup && ! -z $config__paths__local_backup && ! -z $config__ssh__user ]]; then
+# Connect to server via SSH and backup database
+ssh -p "$config__ssh__port" \
+	"$config__ssh__user" \
+	'bash -s' < "$program_dir"/remote.sh \
+		"$config__paths__wordpress" \
+		"$config__paths__remote_backup"
 
-	# Connect to server via SSH and backup database
-	ssh -p 2222 \
-		"$config__ssh__user" \
-		'bash -s' < "$program_dir"/remote.sh \
-			"$config__paths__wordpress" \
-			"$config__paths__remote_backup"
-
-	# Download exported database file
-	scp -P 2222 \
-		"$config__ssh__user":"$config__paths__remote_backup" \
-		"$config__paths__local_backup"
-
-else
-
-	echo SAD
-
-fi
+# Download exported database file
+scp -P "$config__ssh__port" \
+	"$config__ssh__user":"$config__paths__remote_backup" \
+	"$config__paths__local_backup"
