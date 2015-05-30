9104f460e58c40472031bebb1987a0d24a68f88f
Caleb Evans <caleb@calebevans.me>
2015-05-30 12:09:08 -0700

    Run tests using version-agnostic nosetests command

--- tasks.py
+++ tasks.py
@@ -7,12 +7,12 @@ from invoke import task
 @task
 def test():
     # invoke.run() does not color output, unfortunately
-    nosetests = subprocess.Popen(['nosetests-3.4', '--rednose'])
+    nosetests = subprocess.Popen(['nosetests', '--rednose'])
     nosetests.wait()
 
 
 @task
 def cover():
-    nosetests = subprocess.Popen(['nosetests-3.4', '--with-coverage',
+    nosetests = subprocess.Popen(['nosetests', '--with-coverage',
                                   '--cover-erase', '--cover-html'])
     nosetests.wait()
