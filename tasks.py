eea0f026a8fed261283c081d7bc447ec480ff6e5
Caleb Evans <caleb@calebevans.me>
2015-06-12 10:11:04 -0700

    Fix bug that raised error when generating coverage

--- tasks.py
+++ tasks.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 from invoke import task
 
 
 @task
 def test():
-    # invoke.run() does not color output, unfortunately
+    # invoke.run() does not respect colored output, unfortunately
     nosetests = subprocess.Popen(['nosetests', '--rednose'])
     nosetests.wait()
 
 
 @task
 def cover():
+    try:
+        os.mkdir('cover')
+    except OSError:
+        pass
     nosetests = subprocess.Popen(['nosetests', '--with-coverage',
                                   '--cover-erase', '--cover-html'])
     nosetests.wait()
