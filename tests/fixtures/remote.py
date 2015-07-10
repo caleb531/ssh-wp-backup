c5f4db627b053aaeafd8fd33d07715a2b01af4fc
Caleb Evans <caleb@calebevans.me>
2015-07-10 14:38:45 -0700

    Rename src package to swb

--- tests/fixtures/remote.py
+++ tests/fixtures/remote.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import src.remote
+import subprocess
 from unittest.mock import Mock, mock_open, patch
 
 
@@ -24,7 +24,7 @@ def set_up():
     patch_rmdir.start()
     patch_getsize.start()
     patch_popen.start()
-    patch_open = patch('src.remote.open',
+    patch_open = patch('swb.remote.open',
                        mock_open(read_data=WP_CONFIG_CONTENTS), create=True)
     patch_open.start()
 
@@ -34,6 +34,6 @@ def tear_down():
     patch_remove.stop()
     patch_rmdir.stop()
     patch_getsize.stop()
-    src.local.subprocess.Popen.reset_mock()
+    subprocess.Popen.reset_mock()
     patch_popen.stop()
     patch_open.stop()
